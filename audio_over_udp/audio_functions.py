1721d9931341dd7209a4617d2837111e89a5a46c
Ivica Matic <ivica.m@sundance.com>
2020-08-01 23:00:35 +0100

    Fixed audio becoming delayed after long time

--- audio_over_udp/audio_functions.py
+++ audio_over_udp/audio_functions.py
@@ -5,10 +5,8 @@ import Fifo
 import config
 
 _received_queue = Fifo.Fifo()
-silence = chr(0) * config.CHUNK * 4
-print(len(silence))
-
 _sending_queue = Fifo.Fifo()
+silence = chr(0) * config.CHUNK * 4
 
 
 def receive_audio(audio_stream, socket_connection, chunk=config.CHUNK):
@@ -23,7 +21,7 @@ def send_audio(audio_stream, socket_connection, role, chunk=config.CHUNK):
     t1 = threading.Thread(target=record_audio, args=(audio_stream,))
     t1.start()
     while True:
-        if len(_sending_queue):
+        while len(_sending_queue):
             if role == "CLIENT":
                 socket_connection.sendto(_sending_queue.get(chunk),
                                          (config.Node1.NODE2_IP, config.Node1.NODE2_PORT))
@@ -35,10 +33,9 @@ def send_audio(audio_stream, socket_connection, role, chunk=config.CHUNK):
 
 def play_audio(audio_stream):
     while True:
-        if len(_received_queue):
+        while len(_received_queue):
             audio_stream.write(bytes(_received_queue.get(len(_received_queue))))
-        else:
-            audio_stream.write(silence)
+        audio_stream.write(silence)
         time.sleep(1 / config.RATE)
 
 
