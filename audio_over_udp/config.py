88fcb963bfba1c1991f81f7952cafb8ad0eb32f5
IcE <ivica-matic@outlook.com>
2020-08-02 03:00:26 +0100

    Updated config for test

--- audio_over_udp/config.py
+++ audio_over_udp/config.py
@@ -21,10 +21,12 @@ Configuration for client side
 class Node1:
     # Node 1 must know details of Node 2, so put details of Node 2 here
     NODE2_IP = "192.168.1.107"
+    # NODE2_IP = "localhost"
     NODE2_PORT = 8081
 
 
 class Node2:
     # Node 2 must know details of Node1, so put details of Node 1 here
-    NODE1_IP = "192.168.1.104"
+    NODE1_IP = "192.168.1.105"
+    # NODE1_IP = "localhost"
     NODE1_PORT = 8082
